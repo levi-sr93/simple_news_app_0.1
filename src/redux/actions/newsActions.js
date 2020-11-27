c83bb92edf7c561354214acc30404c47e3722505
levi-sr93 <levi-sr@outlook.com>
2020-11-27 12:45:58 -0300

    Making Http requests| Dispatch actions| Fetching Data with Fecth API| Sending data as payload

--- src/redux/actions/newsActions.js
+++ src/redux/actions/newsActions.js
@@ -2,8 +2,16 @@ export const FETCH_ARTICLES = "FETCH_ARTICLES";
 export const ADD_FAVORITES = "ADD_FAVORITES";
 
 export const fetchArticles = () => {
-  return {
-    type: FETCH_ARTICLES,
-    payload: { id: 1, title: "Sport News", description: "Sport new is on" },
+  return async (dispatch) => {
+    //logic to fetch data 
+    const result = await fetch('http://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=81b613598668492493f4adb02f3e6e1b');
+
+    //converting the result from the API to json format
+    const resultData = await result.json();
+
+    dispatch({
+      type: FETCH_ARTICLES,
+      payload: resultData,
+    });
   };
 };
