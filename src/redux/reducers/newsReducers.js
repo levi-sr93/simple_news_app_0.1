f6c783c6fd4f62ee1a2a50ce58afa1c7a80c5ac1
levi-sr93 <levi-sr@outlook.com>
2020-12-02 12:38:55 -0300

    Setting up the newsReducer logic to toggle favorites

--- src/redux/reducers/newsReducers.js
+++ src/redux/reducers/newsReducers.js
@@ -1,18 +1,43 @@
-import { FETCH_ARTICLES } from "../actions/newsActions";
+import { FETCH_ARTICLES, TOGGLE_FAVORITES } from "../actions/newsActions";
 
 const initialState = {
   articles: [],
-  favorites: []
-}
+  favorites: [],
+};
 
-export default function(state = initialState, action){
-  switch(action.type) {
+export default function (state = initialState, action) {
+  switch (action.type) {
     case FETCH_ARTICLES:
       return {
-        ...state, 
-        articles: action.payload
+        ...state,
+        articles: action.payload,
+      };
+
+    case TOGGLE_FAVORITES:
+      //logic to add or remove items from favorites
+
+      const index = state.favorites.findIndex(
+        (article) => article.url === action.payload
+      ); //will return the item index case it exists
+
+      //if the item is not found it will return -1
+      if (index >= 0) {
+        const favorites = [...state.favorites];
+        favorites.splice(index, 1);
+        return {
+          ...state,
+          favorites: favorites,
+        };
+      } else {
+        const article = state.articles.find(article => article.url === action.payload);
+
+        return {
+          ...state,
+          favorites: state.favorites.concat(article)
+        }
       }
   }
 
   return state;
 }
+
