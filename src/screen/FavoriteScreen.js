5d45a4732020b8db78d1947b7cea4148a5746185
levi-sr93 <levi-sr@outlook.com>
2020-12-02 16:48:30 -0300

    Showing flatlist from favorites screen

--- src/screen/FavoriteScreen.js
+++ src/screen/FavoriteScreen.js
@@ -1,21 +1,30 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React from "react";
+import { FlatList, StyleSheet } from "react-native";
+import Card from "../Components/Card";
 
-import {useSelector} from 'react-redux';
 
-const FavoriteScreen = () => {
+import { useSelector } from "react-redux";
 
-  const favorites = useSelector(state => state.news.favorites);
+const FavoriteScreen = (props) => {
+  const favorites = useSelector((state) => state.news.favorites);
 
   return (
-    <View>
-      <Text>Favorite Screen</Text>
-    </View>
-  )
-}
+    <FlatList
+      data={favorites}
+      keyExtractor={(item) => item.url}
+      renderItem={({ item }) => (
+        <Card
+          navigation={props.navigation}
+          title={item.title}
+          image={item.urlToImage}
+          description={item.description}
+          url={item.url}
+        />
+      )}
+    />
+  );
+};
 
-const styles = StyleSheet.create({
-
-});
+const styles = StyleSheet.create({});
 
 export default FavoriteScreen;
