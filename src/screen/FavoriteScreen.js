c47742a1281b11470fcb4a745bf7765267d788bf
levi-sr93 <levi-sr@outlook.com>
2020-12-02 16:37:11 -0300

    useSelector to fetch data from the global store| useSelector

--- src/screen/FavoriteScreen.js
+++ src/screen/FavoriteScreen.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 
+import {useSelector} from 'react-redux';
+
 const FavoriteScreen = () => {
+
+  const favorites = useSelector(state => state.news.favorites);
+
   return (
     <View>
       <Text>Favorite Screen</Text>
